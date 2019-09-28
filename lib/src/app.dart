38bd14036990e3b6847981919ba020ffb9c6edb7
SengHyun Lee <leesnhyun@gmail.com>
2019-09-29 04:48:08 +0900

    [UI][Android] Add Splash

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+      ),
+    );
+  }
+}
