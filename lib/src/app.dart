56f7b010f21947f0ee9105460da27407038d2544
SengHyun Lee <leesnhyun@gmail.com>
2019-09-29 13:22:44 +0900

    [Common] Setting i18n text (KO, EN)

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,9 @@
 import 'package:deal/main.dart';
-import 'package:flutter/material.dart';
+import 'package:deal/generated/i18n.dart';
 import 'package:deal/src/builders/conditional_builder.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'screens/intro/intro.dart';
 import 'screens/login/login.dart';
@@ -9,10 +12,44 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bool isSecondRun = sharedPreferences.getBool("isSecondRun") ?? false;
-    return ConditionalBuilder(
-      conditional: !isSecondRun,
-      truthyBuilder: () => IntroPage(),
-      falsyBuilder: () => LoginPage()
+
+    return MaterialApp(
+
+        onGenerateTitle: (BuildContext context) => S.of(context).appName,
+
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales, // i18n
+        localeResolutionCallback: S.delegate.resolution(fallback: new Locale("ko", ""), withCountry: false),
+
+      theme: ThemeData(
+          // Define the default brightness and colors.
+          brightness: Brightness.dark,
+          primaryColor: Colors.lightBlue[800],
+          accentColor: Colors.cyan[600],
+
+          // Define the default font family.
+          fontFamily: 'NanumSquare',
+
+          // Define the default TextTheme. Use this to specify the default
+          // text styling for headlines, titles, bodies of text, and more.
+          textTheme: TextTheme(
+            headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          ),
+
+      ),
+      home: ConditionalBuilder(
+          conditional: !isSecondRun,
+          truthyBuilder: () => IntroPage(),
+          falsyBuilder: () => LoginPage()
+      )
     );
+
   }
 }
