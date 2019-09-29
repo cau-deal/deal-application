1add135db5e479cf5430797cdab43acc5252f753
Seongmin Kim <shieldnet@cau.ac.kr>
2019-09-29 17:54:29 +0900

    [Android][login] Add Login Widget Source Code and validator. (TODO)put into Container

--- lib/src/screens/login_select/login_select.dart
+++ lib/src/screens/login_select/login_select.dart
@@ -16,11 +16,62 @@ class LoginSelectPage extends StatelessWidget {
             color: Colors.black26
         ),
         child: Padding(
-            padding: const EdgeInsets.all(30),
+            padding: const EdgeInsets.all(30);
             child: Align(
               alignment: AlignmentDirectional.bottomCenter,
-            )
+            ),
         )
+    );
+  }
+
+  Widget emailField(bloc){
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot){
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: "deal@deal.com",
+            labelText: "Email Address",
+            errorText: snapshot.error,
+          ),
+          obscureText: true,
+        );
+      }
+    );
+  }
+
+  Widget passwordField(bloc) {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          decoration: InputDecoration(
+              hintText: 'Must contain 8 characters',
+              labelText: 'Password',
+              errorText: snapshot.error
+          ),
+          obscureText: true,
+        );
+      },
+    );
+  }
+
+  Widget submitButton(bloc) {
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text('Login'),
+          color: Colors.blue[700],
+          textColor: Colors.white,
+          onPressed: snapshot.hasData
+              ? bloc.submit
+              : null,
+        );
+      },
     );
   }
 }
